fce2d8e70d49ba03ef32394a5df8104b146b5b60
Artikon90 <artikon90@gmail.com>
2022-11-13 19:49:31 +0300

    Вынос метода в класс, к данным которого он обращается

--- src/Customer.java
+++ src/Customer.java
@@ -44,22 +44,6 @@ public class Customer {
     }
 
     private static double amountFor(Rental rental) {
-        double result = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2)
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (rental.getDaysRented() > 3)
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return rental.getCharge();
     }
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,3 +1,6 @@
+/**
+ * Класс, представляющий данные о прокате фильма.
+ */
 public class Rental {
     private Movie _movie;
     private int _daysRented;
@@ -13,5 +16,25 @@ public class Rental {
 
     public Movie getMovie() {
         return _movie;
+    }
+
+    public double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
     }
 }
