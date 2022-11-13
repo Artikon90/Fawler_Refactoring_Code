e58b4ace56ef49a97750533869bfe086746e2bf1
Artikon90 <artikon90@gmail.com>
2022-11-13 19:28:54 +0300

    Инит коммит

---
+++ src/Customer.java
@@ -0,0 +1,60 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<>();
+
+    public Customer (String name) {
+        _name = name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName () {
+        return _name;
+    }
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "Учет аренды для " + getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+            //определить сумму для каждой строки
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+            // добавить очки для активного арендатора
+            frequentRenterPoints ++;
+            // бонус за аренду новинки на два дня
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    each.getDaysRented() > 1) frequentRenterPoints ++;
+            //показать результаты для этой аренды
+            result += "\t" + each.getMovie().getTitle()+ "\t" +
+                    String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+        //добавить нижний колонтитул
+        result += "Сумма задолженности составляет " +
+                String.valueOf(totalAmount) + "\n";
+        result += "Вы заработали " + String.valueOf(frequentRenterPoints) +
+                " очков за активность";
+        return result;
+    }
+}
